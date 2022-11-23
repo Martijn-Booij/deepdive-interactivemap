cbd5c4615e4c5b91d9a8f24bad4948bb337f2329
ZurichiYT <91051629+ZurichiYT@users.noreply.github.com>
2022-11-23 14:07:11 +0100

    Add files via upload

--- src/index.php
+++ src/index.php
@@ -10,42 +10,40 @@ if(isset($_POST['insert'])){
 if($naam == null && $coords == null && $kilo_pj && null && $lat == null && $lng == null){
         echo 'U forgot to fill something in';
      } else{
-        $insert = $conn->prepare("INSERT INTO `boom` (`naam`, `lat`, `lng`, `kilo_pj`) VALUES (?, ?, ?, ?)");
-        $insert->execute([$naam, $lat, $lng, $kilo_pj]);
-        echo 'Information inserted';
-     }
-     }
-
-$prepare = $conn->prepare("SELECT * FROM boom ORDER BY id");
-$prepare->execute([]);
-?>
+        $insert = $conn->prepare("INSERT INTO `boom` (`naam`, `lat`, `lng`,
+`kilo_pj`) VALUES (?, ?, ?, ?)"); $insert->execute([$naam, $lat, $lng,
+$kilo_pj]); echo 'Information inserted'; } } $prepare = $conn->prepare("SELECT *
+FROM boom ORDER BY id"); $prepare->execute([]); ?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>Leaflet Quick Start Example</title>
         <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="../map/leaflet.css" />
-        <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.78.0/dist/L.Control.Locate.min.css"
-        />
-        <script src="../map/leaflet.js"></script>
-        <script
-            src="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.78.0/dist/L.Control.Locate.min.js"
-            charset="utf-8"
-        ></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.78.0/dist/L.Control.Locate.min.css"/>
+        <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.4/dist/flowbite.min.css" />
+        <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
+        <script src="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.78.0/dist/L.Control.Locate.min.js" charset="utf-8"></script>
+        <script src="https://cdn.tailwindcss.com"></script>
     </head>
     <body>
-        <div id="map" style="width: 100%; height: 600px"></div>
+        <div id="map" class="z-0" style="width: 100%; height: 600px"></div>
 
-        <section class="insert">
-            <form action="" method="POST">
-                <h3>Insert test</h3>
-
-                <div class="flex">
+<!-- Main modal -->
+<div id="authentication-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 p-4 w-full md:inset-0 h-modal md:h-full">
+    <div class="relative w-full max-w-md h-full md:h-auto">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <button type="button" id="close" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white">
+                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+            <div class="py-6 px-6 lg:px-8">
+                <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Voeg een boom toe</h3>
+                <form id="form" class="space-y-6" action="" method="POST">
                     <div class="inputBox">
-                        <span>Boom naam:</span>
+                    <label for="naam" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Boomnaam:</label>
                         <input
                             type="text"
                             name="naam"
@@ -54,75 +52,102 @@ $prepare->execute([]);
                             required
                         />
                     </div>
-                    <div class="inputBox">
-                        <span>latitude :</span>
+                        <label for="lat" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">latitude:</label>
+                        <div class="inputBox">
                         <input
                             type="number"
+                            id="lat"
                             name="lat"
                             step="any"
-                            placeholder="eg. 52.254729"
                             class="box"
                             required
                         />
-                    </div>
-
-                    <div class="inputBox">
-                        <span>lng :</span>
+                        </div>
+                        <label for="lng" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">lng:</label>
+                        <div class="inputBox">
                         <input
                             type="number"
+                            id="lng"
                             name="lng"
                             step="any"
-                            placeholder="eg. 5.041415"
                             class="box"
                             required
                         />
-                    </div>
-
-                    <div class="inputBox">
-                        <span>Kilo per jaar</span>
+                        </div>
+                        <label for="kilo_pj" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Klo per jaar:</label>
+                        <div class="inputBox">
                         <input
                             type="text"
                             name="kilo_pj"
-                            placeholder="enter your kilo per jaaar"
+                            placeholder="enter your kilo per jaar"
                             class="box"
                             required
                         />
-                    </div>
-                </div>
+                        </div>
 
-                <a href="hier bezig"
-                    ><input type="submit" name="insert" value="Insert"
-                /></a>
-            </form>
-        </section>
+                    <div class="flex justify-between">
+                        <div class="flex items-start">
+                    <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" name="insert" value="Insert">Insert information</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div> 
+        <script src="https://unpkg.com/flowbite@1.5.4/dist/flowbite.js"></script>
     </body>
 </html>
 
 <script>
-            var map = L.map('map').setView([52.25459030239216, 5.040947601205085], 18.7);
-        L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
-    maxZoom: 20,
-    subdomains:['mt0','mt1','mt2','mt3']
-    }).addTo(map);
+           var map = L.map('map').setView([52.25459030239216, 5.040947601205085], 18.7);
 
-    var myIcon = L.icon({
-        iconUrl: './images/tree.svg',
-        iconSize: [30, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
+           L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{ noWrap: true,
+       maxZoom: 20,
+       subdomains:['mt0','mt1','mt2','mt3']
+       }).addTo(map);
+
+       var myIcon = L.icon({
+           iconUrl: './images/tree.svg',
+           iconSize: [30, 41],
+           iconAnchor: [12, 41],
+           popupAnchor: [1, -34],
+           shadowSize: [41, 41]
+       });
+
+       var database = <?php echo json_encode($prepare->fetchAll(PDO::FETCH_ASSOC)); ?>;
+       for (var i = 0; i < database.length; i++) {
+           var a = database[i];
+           var naam = a['naam'];
+           var date = a['date_added'];
+           var kilo = a['kilo_pj'];
+           var marker = L.marker([a['lat'], a['lng']], {icon: myIcon}).addTo(map);
+           marker.bindPopup(
+               '<b>Boom naam:</b> ' +
+                   naam +
+                   '<br><b>Date added:</b> ' +
+                   date +
+                   '<br><b>Kilo per jaar:</b> ' +
+                   kilo
+           );
+       }
+
+       map.addEventListener('click', function(e) {
+        var lat = e.latlng.lat;
+        var lng = e.latlng.lng;
+        document.getElementById('lat').value = lat;
+        document.getElementById('lng').value = lng;
+        modal = document.getElementById('authentication-modal');
+        modal.classList.add('active');
+        modal.classList.remove('hidden');
+           }
+);
+
+if (document.getElementById('close')) {
+    document.getElementById('close').addEventListener('click', function() {
+        modal = document.getElementById('authentication-modal');
+        modal.classList.remove('active');
+        modal.classList.add('hidden');
     });
-
-    var database = <?php echo json_encode($prepare->fetchAll(PDO::FETCH_ASSOC)); ?>;
-    for (var i = 0; i < database.length; i++) {
-        var a = database[i];
-        var naam = a['naam'];
-        var date = a['date_added'];
-        var kilo = a['kilo_pj'];
-        var marker = L.marker([a['lat'], a['lng']], {icon: myIcon}).addTo(map);
-        marker.bindPopup(naam + ' ' + date + ' ' + kilo).openPopup();
-    }
-
-    L.control.locate().addTo(map);
-    L.showPopup();
+}
+       L.control.locate().addTo(map);
+       L.showPopup();
 </script>
